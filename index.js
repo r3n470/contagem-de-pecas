914eff3a409346dbfd00537779331f0acced9232
r3n470 <rluiz1808@gmail.com>
2022-07-27 21:23:00 -0300

    corrigida identacao e alterados os pre-requisitos para const

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 //pré-requisitos
-let pesoMinimo = 100;
-let listaMaxima = 10;
-let nomeCorretoDaPeca = "abc";
+const pesoMinimo = 100;
+const listaMaxima = 10;
+const nomeCorretoDaPeca = 'abc';
 
 //informações a serem lançadas
-let pesoDaPeca = 110;
-let listaDePeca = 9;
-let nomeDaPeca = "Retrovisor";
+let pesoDaPeca = 109;
+let listaDePeca = 7;
+let nomeDaPeca = 'retrovisor';
 
 //codigos usados
-if (pesoDaPeca > pesoMinimo) console.log("Pode cadastrar");
-else console.log("Peso incorreto");
-
-if (listaDePeca > listaMaxima) console.log("Capacidade ultrapassada");
-else console.log("Dentro da capacidade");
-
-if (nomeDaPeca.length >= nomeCorretoDaPeca.length) {
-  console.log("Nome válido");
+if (pesoDaPeca > pesoMinimo) {
+    console.log('Pode cadastrar');
 }
 else {
-  console.log("Nome precisa de pelo menos 3 caracteres");
+    console.log('Cadastro não pode ser realizado, peso inferior a 100 gramas');
+}
+
+
+if (listaDePeca > listaMaxima) {
+    console.log('Capacidade ultrapassada');
+}
+else {
+    console.log('Dentro da capacidade');
+}
+
+
+if (nomeDaPeca.length >= nomeCorretoDaPeca.length) {
+    console.log('Nome válido');
+}
+else {
+    console.log('Nome precisa de pelo menos 3 caracteres');
 }
